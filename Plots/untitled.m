39d49eac5378ca38fb2d6d38fc791626dc2a558c
RickReddy <40832092+RickReddy@users.noreply.github.com>
2024-02-24 18:08:21 -0600

    Delete Plots directory
    New PhysioQA file is not specific for one dataset

--- Plots/untitled.m
+++
@@ -1,6 +0,0 @@
-a = which('sub-A00008326');
-
-path = '/data1/neurdylab/datasets/nki_rockland/raw/sub-A00037531/ses-BAS1/func/sub-A00037531_ses-BAS1_task-rest_acq-1400_physio.json';
-if isfile(path)
-    display('yes');
-end
